58c901bf6151127c6d3355f641801cd3e1847637
deva <devashishdewalkar@gmail.com>
2024-08-06 16:56:36 +0530

    initialize server with json response

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
+type Schema struct {
+	Id   string
+	Name string
+}
+
 func main() {
-	fmt.Println("xkd!")
+	fmt.Println("Starting GO Server!")
+
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("=> ", r.URL, r.Host)
+		dbSchema := Schema{
+			Id:   "0",
+			Name: "schema0",
+		}
+		data, err := json.Marshal(dbSchema)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("response: ", data)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(data)
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Err: ", err)
+	}
 }
