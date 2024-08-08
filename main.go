e12556102d3a64c654135eb21a20600c368bb3f9
deva <devashishdewalkar@gmail.com>
2024-08-08 18:09:21 +0530

    add POST method to record key-value pairs

--- main.go
+++ main.go
@@ -6,29 +6,51 @@ import (
 	"net/http"
 )
 
-type Schema struct {
-	Id   string
-	Name string
+type tableSchema struct {
+	Key   string
+	Value string
 }
+
+var list = []tableSchema{}
 
 func main() {
 	fmt.Println("Starting GO Server!")
 
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("=> ", r.URL, r.Host)
-		dbSchema := Schema{
-			Id:   "0",
-			Name: "schema0",
+
+		newEntry := tableSchema{
+			Key:   "0",
+			Value: "Hello",
 		}
-		data, err := json.Marshal(dbSchema)
+		data, err := json.Marshal(newEntry)
 		if err != nil {
 			fmt.Println(err)
+		} else {
+			fmt.Println("response: ", data)
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(200)
+			w.Write(data)
+		}
+	})
+
+	http.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Println("=> ", r.URL, r.Body)
+		var entry tableSchema
+		err := json.NewDecoder(r.Body).Decode(&entry)
+
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(500)
+			w.Write([]byte(`{"key" : -1, "value": "failed!"}`))
+		} else {
+			list = append(list, entry)
+			fmt.Println("received: ", list)
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(201)
+			w.Write([]byte(`{"key" : 0, "value": "received!"}`))
 		}
 
-		fmt.Println("response: ", data)
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(200)
-		w.Write(data)
 	})
 
 	err := http.ListenAndServe(":8080", nil)
